17560679c1d72ccbdae247181e53e473fdc0ea0f
SarCasm <sarcasm008@gmail.com>
2015-10-28 21:51:47 +0200

    (⓪益⓪)

---
+++ dont_look_here.rb
@@ -0,0 +1,4 @@
+# 2 lz 2 chng exmpl.rb
+def p arg
+  puts ("\t" << arg.inspect)  # ugly => fancy
+end
--- example.rb
+++ example.rb
@@ -1,5 +1,5 @@
 require_relative 'factory'
-
+require_relative 'dont_look_here'
 #      ______           __
 #     / ____/___ ______/ /_____  _______  __
 #    / /_  / __ `/ ___/ __/ __ \/ ___/ / / /
@@ -8,9 +8,9 @@ require_relative 'factory'
 #                                  /____/
 
 # Should be no errors
-p Factory.new("Customer", :name, :address)
+puts Factory.new("Customer", :name, :address)
 #=> Factory::Customer
-p Factory::Customer.new("Dave", "123 Main")
+puts Factory::Customer.new("Dave", "123 Main")
 #=> #<Factory::Customer:0x00000001bfbc50 name="Dave", address="123 Main">
 
 SomeCustomer = Factory.new(:name, :address) do
@@ -18,9 +18,9 @@ SomeCustomer = Factory.new(:name, :address) do
     "Hello #{name}!"
   end
 end
-p SomeCustomer.new("Dave", "123 Main").greeting  # => "Hello Dave!"
+puts SomeCustomer.new("Dave", "123 Main").greeting  # => "Hello Dave!"
 
-p "struct == other → true or false"
+puts "struct == other → true or false"
   Customer = Factory.new(:name, :address, :zip)
     joe   = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
     joejr = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
@@ -28,15 +28,15 @@ p "struct == other → true or false"
   p joe == joejr   #=> true
   p joe == jane    #=> false
 
-p "struct[member] → anObject"
-p "struct[index] → anObject"
+puts "struct[member] → anObject"
+puts "struct[index] → anObject"
   p joe["name"]   #=> "Joe Smith"
   p joe[:name]    #=> "Joe Smith"
   p joe[0]        #=> "Joe Smith"
 
 
-p "struct[name] = obj → obj"
-p "struct[index] = obj → obj"
+puts "struct[name] = obj → obj"
+puts "struct[index] = obj → obj"
   joe["name"] = "Luke"
   joe[1]      = "some st."
   joe[:zip]   = "90210"
@@ -46,44 +46,44 @@ p "struct[index] = obj → obj"
   p joe.zip       #=> "90210"
 
 
-p "each {|obj| block } → struct"
-p "each → an_enumerator"
+puts "each {|obj| block } → struct"
+puts "each → an_enumerator"
   joe   = Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)
-  joe.each {|x| puts(x) }
+  joe.each {|x| p(x) }
   # Produces:
   # Joe Smith
   # 123 Maple, Anytown NC
   # 12345
 
 
-p "each_pair {|sym, obj| block } → struct"
-p "each_pair → an_enumerator"
-  joe.each_pair {|name, value| puts("#{name} => #{value}") }
+puts "each_pair {|sym, obj| block } → struct"
+puts "each_pair → an_enumerator"
+  joe.each_pair {|name, value| p("#{name} => #{value}") }
   # Produces:
   # @name => Joe Smith
   # @address => 123 Maple, Anytown NC
   # @zip => 12345
 
-p "select {|i| block } → array"
-p "select → an_enumerator"
+puts "select {|i| block } → array"
+puts "select → an_enumerator"
   Lots = Struct.new(:a, :b, :c, :d, :e, :f)
   l = Lots.new(11, 22, 33, 44, 55, 66)
   p l.select {|v| (v % 2).zero? }   #=> [22, 44, 66]
 
 
-p "all? {|i| block } → true or false"
-p "all? → an_enumerator"
+puts "all? {|i| block } → true or false"
+puts "all? → an_enumerator"
   p l.all? {|v| v>15 }        # => false
 
-p "any? {|i| block } → true or false"
-p "any? → an_enumerator"
+puts "any? {|i| block } → true or false"
+puts "any? → an_enumerator"
   p l.any? {|v| v.eql? 11 }   # => true
 
-p "size → fixnum"
+puts "size → fixnum"
   p joe.length   #=> 3
 
 
-p "Array#uniq → #hash works"
+puts "Array#uniq → #hash works"
   z   = [Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345),
          Customer.new("Joe Smith", "123 Maple, Anytown NC", 12345)]
   p z.uniq
