4541124bdea67b9a48cfe90f6f4abde82c20a32d
SarCasm <sarcasm008@gmail.com>
2015-10-29 09:53:57 +0200

    RUBOCOPED THE S*** OUT OF ME

--- factory.rb
+++ factory.rb
@@ -1,46 +1,45 @@
+# Fancy Factory
 class Factory
-  def self.new *args, &block
-    if args[0].is_a?(String)
-      class_name = args.shift.capitalize
-    end
+  def self.new(*args, &block)
+    class_name = args.shift.capitalize if args[0].is_a?(String)
     new_class = Class.new do
       args.each { |x| attr_accessor x }
 
       define_method :initialize do |*attr|
-        attr.each.with_index do |x,i|
+        attr.each.with_index do |x, i|
           instance_variable_set("@#{args[i]}", x)
         end
       end
 
       # Basic stuff
-      def == (other)
-        if other.kind_of?(self.class)
-          instance_hash == other.instance_hash
-        end
+      def ==(other)
+        instance_hash == other.instance_hash if other.ia_a?(self.class)
       end
-      def eql? other
+
+      def eql?(other)
         self == other
       end
+
       def hash
         instance_hash.hash
       end
 
       # Array-like getter
-      def [] index
+      def [](index)
         case index
         when String, Symbol
-          self.instance_variable_get("@" << index.to_s)
+          instance_variable_get('@' << index.to_s)
         when Fixnum
-          self.instance_variable_get(instance_variables[index])
+          instance_variable_get(instance_variables[index])
         end
       end
       # Array-like setter
-      def []= index, value
+      def []=(index, value)
         case index
         when String, Symbol
-          self.instance_variable_set("@" << index.to_s, value)
+          instance_variable_set('@' << index.to_s, value)
         when Fixnum
-          self.instance_variable_set(instance_variables[index], value)
+          instance_variable_set(instance_variables[index], value)
         end
       end
 
@@ -52,41 +51,42 @@ class Factory
       alias_method :count, :size
 
       def instance_values
-        self.instance_variables.map do |var|  # => [:name, :age]
-          self.instance_variable_get(var)     # => ['John', 42]
+        instance_variables.map do |var|  # => [:name, :age]
+          instance_variable_get(var)     # => ['John', 42]
         end
       end
+
       def instance_hash
-        self.instance_variables.inject(Hash.new) do |hash,var|
-          hash[var] = self.instance_variable_get(var)   # => value
-          hash                                          # => a_hash
+        instance_variables.each_with_object({}) do |var, hash|
+          hash[var] = instance_variable_get(var)
         end
       end
 
       # Enumerables
       include Enumerable
-      def each &block
-        instance_values.each &block
+      def each(&block)
+        instance_values.each(&block)
       end
-      def each_pair &block
-        instance_hash.each_pair &block
+
+      def each_pair(&block)
+        instance_hash.each_pair(&block)
       end
 
       # Output
       def inspect
-        super.gsub(/@/,"")  # get out!!11
-                            # \_O__
-                            #  \__
-                            #  |  \,@
-                            # /
+        super.gsub(/@/, '')  # get out!!11
+                             # \_O__
+                             #  \__
+                             #  |  \,@
+                             # /
       end
       alias_method :to_s, :inspect
 
       # Block call
-      class_eval &block if block_given?
+      class_eval(&block) if block_given?
     end
 
-    self.const_set(class_name, new_class) if class_name
-    return new_class
+    const_set(class_name, new_class) if class_name
+    new_class
   end
 end
